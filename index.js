cf63d7cc8a05f749f6c7b879e881bbc0d2c3548a
wtscott@gmail.com <wtscott@gmail.com>
2018-04-21 23:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-  
-function theBeatlesPlay(instruments, musicians) {
+function theBeatlesPlay(musicians, instruments) {
   var array = []
   for(let i=0; i<musicians.length; i++) {
     array.push(musicians[i] +" plays "+instruments[i])
